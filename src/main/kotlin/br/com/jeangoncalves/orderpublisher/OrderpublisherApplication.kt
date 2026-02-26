a083ba3db1050a8f71a43c328b3ed14536b5e8b8
jeanrgoncalves <jeanrgoncalves@hotmail.com>
2026-02-26 07:50:55 -0300

    pimeiro commit

---
+++ src/main/kotlin/br/com/jeangoncalves/orderpublisher/OrderpublisherApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jeangoncalves.orderpublisher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderpublisherApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderpublisherApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/jeangoncalves/orderpublisher/OrderpublisherApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jeangoncalves.orderpublisher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderpublisherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
